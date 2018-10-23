f96a8618f4f1df6b35472a9ecc8434098efaad60
Jay Vyas <jvyas@blackducksoftware.com>
2018-10-23 17:36:52 -0400

    rob rati how do i get deployer.go to compile?

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/manifoldco/promptui"
+)
+
+func horizonExample() {
+
+}
+
+func promptuiExample() {
+	prompt := promptui.Select{
+		Label: "Select Day",
+		Items: []string{
+			"Monday",
+			"Tuesday",
+			"Wednesday",
+			"Thursday",
+			"Friday",
+			"Saturday",
+			"Sunday",
+		},
+	}
+	_, result, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+	fmt.Printf("You choose %q\n", result)
+}
