99926d312931757f63e89cf7db309f5b06ceacb5
Jay Vyas <jvyas@blackducksoftware.com>
2018-10-24 14:41:12 -0400

    cobra subcommand

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/jayunit100/blackduck-ctl/pkg/interactive2"
-	"github.com/spf13/viper"
-)
-
-type Blackduckctl struct {
-	LaunchUI bool
-}
+import "github.com/jayunit100/blackduck-ctl/pkg/apps"
 
 func main() {
-	viper.ReadInConfig()
-	if err := viper.ReadInConfig(); err != nil {
-		fmt.Println("Can't read config:", err)
-		os.Exit(1)
-	}
-	config := &Blackduckctl{}
-	viper.Unmarshal(config)
-
-	if config.LaunchUI {
-		interactive2.Launch()
-	}
-
+	apps.Execute()
 }
